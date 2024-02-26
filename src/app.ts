f20e549b9674ef0252ea3fcd9e23fdddd552f4a2
Paoo08 <paola.permz@gmail.com>
2024-02-26 14:13:20 -0600

    user

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,33 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import { logErrors, errorHandler, boomErrorHandler } from './middlewares/error.handler'
+import {
+  logErrors,
+  errorHandler,
+  boomErrorHandler
+} from './middlewares/error.handler'
 import RouterApi from './routes'
 import router from './routes/homework.route'
 import routerApi from './routes'
-import {config} from './config/config'
+import { config } from './config/config'
 
-const {mongoUri, port} = config
+const { mongoUri, port } = config
 
 const app = express()
 
 const connectDB = () => {
-    mongoose.connect(mongoUri)
+  mongoose.connect(mongoUri)
 }
 
 app.use(express.json())
 routerApi(app)
 
 app.get('/', (req, res) => {
-    res.send('Bye Bye World')
+  res.send('Bye Bye World')
 })
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
-    connectDB()
+  console.log(`Server is running at http://localhost:${port}`)
+  connectDB()
 })
 
 app.use(logErrors)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import HomeworkRouter from './homework.route';
+import express from 'express'
+import HomeworkRouter from './homework.route'
+import UserRouter from './user.route'
 
 const routerApi = (app) => {
     const router = express.Router()
     app.use('/api/v1', router)
     router.use('/homeworks', HomeworkRouter)
+    router.use('/users', UserRouter)
 }
 
 export default routerApi
